53594410a35fd1d0739d0cbe4d3615c14d68fcf3
briannemcgrath <113979868+briannemcgrath@users.noreply.github.com>
2025-04-19 14:43:05 +0100

    Task Three Update - Corrections on Data Fetching
    - Updated URL from CSV data to JSON as initially requested.
    - Cso.json file updated to expected output.

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -1,9 +1,9 @@
 import requests
 import json 
 
-url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/CSV/1.0/en"
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
 response = requests.get(url)
-data = response.text
+data = response.json()
 
 with open ('cso.json', 'w') as f:
-    f.write(data)
+    json.dump(data,f)
